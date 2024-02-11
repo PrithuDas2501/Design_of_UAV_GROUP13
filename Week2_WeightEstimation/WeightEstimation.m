22d804d8872d989885e544df435ac1fc78fcb95a
Hrishav Das <72787333+PrithuDas2501@users.noreply.github.com>
2024-02-11 17:01:53 +0530

    Update WeightEstimation.m

--- Week2_WeightEstimation/WeightEstimation.m
+++ Week2_WeightEstimation/WeightEstimation.m
@@ -1,8 +1,8 @@
 %% Entering Data and visualizing it (WE NEED TO DO SOMETHING ABOUT THIS!!!!!)
 %Empty_Weight = [8, 5, 3.7, 12, 14.9, 1.95, 11.5, 1.5, 6]; 
 %MTOW = [9.5, 10, 4.7, 18, 21.5, 5.9, 14.5, 2.2, 10];
-MTOW = [10,27,5.5,35,12,7];
-Empty_Weight = [4.4,14,3.5,20,9,4.2];
+MTOW = [10,27,5.5,35,12,9,5,26];
+Empty_Weight = [4.4,14,3.5,20,9,6.2,3.5,8.7];
 
 
 p = polyfit(MTOW, Empty_Weight./MTOW, 2);
@@ -15,53 +15,65 @@ scatter(MTOW,Empty_Weight./MTOW)
 plot(x,y)
 
 %% Finding A, L by curve fitting
-sol = fmincon(@(X) find_AL(X,MTOW,Empty_Weight./MTOW),[1,0]);
+sol = fmincon(@(X) find_AL(X,MTOW,Empty_Weight./MTOW),[1,0])
 find_AL(sol,MTOW,Empty_Weight./MTOW);
 %% Power Calculation
-P = 0;
-% Take off and Landing
-% Taking L/D at cruise to be 7, T = W/7
-% Assuming Vliftoff is 1.15 times Vstall, and assuming that power
-% requirement can be taken by considering the V during the take off run to
-% be 0.7 times Vliftoff.
-% From previous literature, we take Vstall to be Vstall = 12m/s
-Vaveragetakeoff = 0.7*1.15*12;
-W = 10*9.81;
-LbyD = 7;
-T = W/LbyD;
-Ptakeoff = T*Vaveragetakeoff;
-Etakeoff = Ptakeoff*30;
-Energy = 2*Etakeoff;
+%% Energy and Battery Weight Fraction Estimation
+Mass = 10; %(Can be anything, will be cancelled in the final battery weight estimation)
+W = Mass*9.81;
+LbyD = 9;
 
-%P = 
+Ptakeoff = (W/LbyD)*1.2*12;
+Etakeoff = Ptakeoff*30;
+
+P_climb = (12.5/LbyD)*W;
+E_Climb = P_climb*120;
+
+P_CruiseLoit = (20/LbyD)*W;
+E_CruiseLoit = P_CruiseLoit*2*60*60;
+
+Total_Energy = 2*Etakeoff + 2*E_Climb + E_CruiseLoit;
+
+Battery_Energy_Density = 260; % W h/ Kg
+Battery_Weight = Total_Energy/(Battery_Energy_Density*60*60);
+Battery_Weight_Fraction = Battery_Weight/Mass;
+display(Battery_Weight_Fraction);
 %% Iterative Loop to find initial estimate of weight
 A = sol(1);
 L = sol(2);
-W_p = 3; % Kg (We have to find this)
-battery_frac= 0.215; % (We have to find this)
+W_p = 2; % Kg (We have to find this)
+Battery_Weight_Fraction; % (Calculated in Battery Estimation)
 
-w1 = 10; % Kg (Just an initial guess)
-i = 1;
-diff = 10;
 figure
-array = [i w1];
 hold on
 grid on
-while abs(diff)>0.001 
-    empty_frac = find_empty_frac(A,L,w1);
-    w2 = W_p/(1-battery_frac/w1-empty_frac); 
-    diff = (w2-w1)/w1;
-    w1 = w2;
-    i=i+1;
-    array(i,:) = [i w1];
-    if i>100
-        break
+ylabel('Weight')
+xlabel('Iteration Number')
+title('Convergence of Weight')
+n = 20;
+W = linspace(5,15,n);
+for k = 1:n
+    w1 = W(k); % Kg (Just an initial guess)
+    i = 1;
+    diff = 10;
+    array = zeros(1,2);
+    array = [i w1];
+    while abs(diff)>0.00001 
+        empty_frac = find_empty_frac(A,L,w1);
+        w2 = W_p/(1-Battery_Weight_Fraction-empty_frac); 
+        diff = (w2-w1)/w1;
+        w1 = w2;
+        i=i+1;
+        array(i,:) = [i w1];
+        if i>100
+            break
+        end
     end
+    % display(w1)
+    plot(array(:,1),array(:,2), 'Color','b')
 end
-plot(array(:,1),array(:,2))
 
-display(w1)
-
+%%
 function y = find_empty_frac(A,L,W)
 y = A*W.^L;
 end
@@ -73,4 +85,3 @@ y = A*MW.^L;
 error = (frac-y)./y;
 cost = error*error';
 end
-
